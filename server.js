d1cb98afcc1275382aff0b35d8e176941732d080
hiep011094 <hiep011094@gmail.com>
2022-11-13 17:21:48 +0700

    hiep

--- server.js
+++ server.js
@@ -32,10 +32,7 @@ app.use(fileUpload({
 
 // Connect Monggose
 const URI = process.env.DB
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-},err => {
+mongoose.connect(URI,err => {
     if(err) throw err
     console.log("Connected to mongodb")
 })
